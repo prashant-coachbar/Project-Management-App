5a22e3f8982859c44b7cae2ff599c58d31f94160
prashant-coachbar <prashant.kumar@coachbar.io>
2025-02-10 13:52:12 +0530

    text changes

--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -21,7 +21,7 @@ export default function NoProject() {
         onClick={addProject}
         className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
       >
-        Create New Project
+        Create New Projects
       </button>
     </div>
   );
